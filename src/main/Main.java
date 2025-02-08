6f4af27668015d423828721654bef50884d73cf7
Augusto <senescal.oliveira@gmail.com>
2025-02-08 09:35:43 -0300

    fix: ajuste na classe Main

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,7 +3,7 @@ package main;
 import produto.Produto;
 
 public class Main {
-    public static void Main(String[] args){
+    public static void main(String[] args){
 
         Produto produto = new Produto(1, "Notebook", 5300.35);
         System.out.println(produto.obterDetalhes());
