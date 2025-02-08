2e1a0d3bc9adc4de82a2993d042459f1ec30f818
Augusto <senescal.oliveira@gmail.com>
2025-02-08 09:33:49 -0300

    feat: adição do cadastro de novo produto

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,12 @@
 package main;
 
+import produto.Produto;
+
 public class Main {
     public static void Main(String[] args){
+
+        Produto produto = new Produto(1, "Notebook", 5300.35);
+        System.out.println(produto.obterDetalhes());
 
     }
 }
